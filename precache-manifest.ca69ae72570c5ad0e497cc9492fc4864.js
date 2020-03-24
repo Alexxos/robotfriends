ceee25ffd35c854868f75c717d6e98ebc974a24a
Alexxos <alexos.droid@gmail.com>
2020-03-24 15:55:48 +0700

    Updates

---
+++ precache-manifest.ca69ae72570c5ad0e497cc9492fc4864.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3dd0f5e9f60f59e114c025e05f09a8",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "04ab7fc80f3404cbfdc3",
+    "url": "/robotfriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d08f7d94fd3f32f46d6f",
+    "url": "/robotfriends/static/css/main.5641d309.chunk.css"
+  },
+  {
+    "revision": "04ab7fc80f3404cbfdc3",
+    "url": "/robotfriends/static/js/2.fa39dd7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends/static/js/2.fa39dd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08f7d94fd3f32f46d6f",
+    "url": "/robotfriends/static/js/main.0aed1896.chunk.js"
+  },
+  {
+    "revision": "78ca9c88f25684aefc9a",
+    "url": "/robotfriends/static/js/runtime-main.da8d021c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
